051c292589c5b938430cef54b6ad6c60b96b75a7
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-10-21 03:38:14 -0700

    Add files via upload

---
+++ 21.js
@@ -0,0 +1,39 @@
+var newarray = ["esha",1,3,4,5,"jayanth"]
+console.log(newarray)
+var numarr = [1,2,3,4,5,6]
+var stringarr = ["esha","samara","trisha"]
+
+console.log(stringarr)
+console.log(numarr)
+
+var combine = [...numarr,...stringarr]
+console.log(combine)
+
+var mixedarr = [21,33,44,"tata","sumo"]
+
+// accessing the elements in an array...
+console.log(mixedarr[2])
+
+//array methods.
+//push
+// pop
+// shift
+// unshift
+//length
+//splice() 
+//contact()
+//slice()
+// foreach
+// map
+//filter
+
+const end = [1,2,3,4,5,6,7,8,9]
+console.log(end.push())
+console.log(end.pop())
+//new contribution
+
+// i dont even know what the fuck is happening..
+
+// ohh iguess it works this way or may be i don't know....
+
+
