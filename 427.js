4ff6e03ecc21618a7640fe3711030d014460ecd4
hemanthlbt <buntytwo@proton.me>
2024-11-12 16:44:33 +0530

    test1

---
+++ 427.js
@@ -0,0 +1,9 @@
+var marvel = ["spiderman","hulk","ironman","thor"];
+var justicel = ["batman","flash","aquaman","wonderwomen",""];
+
+console.log(justicel.length)
+
+
+console.log("test")
+console.log("to the terminal")
+console.log("track this shit")
