fae9f0566f50c114207572783d58f2210b1bbaf5
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-13 13:03:14 +0530

    Add files via upload

---
+++ indes.js
@@ -0,0 +1,18 @@
+// string objects.
+
+let string  = "thisishemanthkumar"
+//console.log(typeof string)
+// string.trimStart()
+// console.log(string)
+
+// console.log(string.toUpperCase())
+// console.log(string.toLowerCase())
+
+//to join two or more strings.
+
+let str = "hemanthkumar"
+let str2 = "lingabathina"
+
+// console.log(...str,...str2)
+
+
