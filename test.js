f073790257bd751e70d1d65673580d913865e0c2
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-13 17:54:01 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -17,3 +17,29 @@ people[3] = "harinarayanan"
 
 
 // console.log(people)
+
+
+// let me make this another one of a nice one code for a nice small green dot on my git hub.
+// THis shall go to my private repo man.
+// I think someone is going on private repo
+
+
+let indian_cities = ["new","bhagya","bombay","puna","bhuvanesh","koll","sennai","bhagyanagar","kochi","benglore"]
+indian_cities[0] =  "newdelhi",
+indian_cities[1] = "hyderabad",
+indian_cities[2] = "mumbai",
+indian_cities[3] = "pune",
+indian_cities[4] = "orrisa",
+indian_cities[5] = "kollkata",
+indian_cities[6] ="chennai",
+indian_cities[7] = "hyderabad",
+indian_cities[8] ="kochin,kerala",
+indian_cities[9] ="banglore,karnataka"
+
+
+console.log(indian_cities)
+
+
+
+
+
