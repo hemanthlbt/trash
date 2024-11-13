d8a9c37586da1574d02d47067080b9283a84987c
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-13 17:47:59 +0530

    Add files via upload

---
+++ lastarrays.js
@@ -0,0 +1,83 @@
+const array = ["apple","banaga","carrot","dryfruit","eggplant"]
+
+const mixedarray = ["null","undefined","true","hello",1]
+// console.log(mixedarray)
+
+//accessing the array elements.
+
+// console.log(mixedarray[0])
+//array.push
+// mixedarray.push("hemanth")
+// console.log(mixedarray)
+// mixedarray.pop()
+// console.log(mixedarray)
+// mixedarray.shift()
+// console.log(mixedarray)
+// mixedarray.unshift()
+// console.log(mixedarray)
+//push,pop,shift,unshift,length,splice(),concat(),slice(),foreach,map,filter.
+
+// 1.push
+// 2.pop
+// 3.shift
+// 4.unshift
+// 5.splice
+// 6.concat()
+// 7.slice()
+// 8.for each 
+// 9.map
+// 10.filter
+// 11.length
+
+const names_array = ["hemanth","samara","esha","trisha","kansasra","deepika","haniaamir"]
+const new_names = ["prabhas","ntr","ramcharan","pavankalyan","maheshbabu","rampothneni"]
+// names_array.push("siakumar")
+// console.log(names_array)
+
+// names_array.pop()
+
+// console.log(names_array)
+
+// names_array.shift()
+// console.log(names_array)
+
+// names_array.unshift()
+// console.log(names_array)
+
+// names_array.splice(5)
+// console.log(names_array)
+
+// const all = names_array.concat(new_names)
+// console.log(all)
+
+// const numbers = names_array.slice(1,5);
+// console.log(numbers)
+
+// console.log(names_array)
+
+// console.clear()
+
+// const again = names_array.slice(-3)
+// console.log(again)
+
+
+let another_one = ["apples","bananas","cashews"]
+
+another_one[0] = "shimla",
+another_one[1] = "africa",
+another_one[2] = "hemanth"
+
+
+console.log(another_one)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,19 @@
+let participants = ["samara","eshanarang","eshakansara","trishajawahar"]
+participants[0] = "samarasaigalluthra",
+participants[1] = "eshanarang",
+participants[2] = "eshakansara gujju",
+participants[3] = "trishajawahar"
+
+// console.log(participants)
+
+
+
+
+let people = ["esha","samara","trisha","hari"]
+people[0] ="eshanarang",
+people[1] = "samarasaigal",
+people[2] = "trishajawahar",
+people[3] = "harinarayanan"
+
+
+// console.log(people)
