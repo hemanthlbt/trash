a26b6ec1677c3255524b41d3c1e47fba5caec80a
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-17 20:19:38 +0530

    Add files via upload

---
+++ jayanth.js
@@ -0,0 +1,17 @@
+let elements = [1,2,3,4,5];
+let elements2= [2,3,4,5,6];
+
+
+console.log("github");
+console.log("github account");
+
+console.log("git init")
+console.log("git add.")
+console.log(`git commit -m "randomname".`)
+console.log("git remote add origin master https:/github.com/random_repoaccount")
+console.log("git push -u origin main")
+console.log("man i am scared for my life.")
+
+
+
+
